c642833309c2a16ec128297cb215cf2599272199
Cix8 <francesco.natal_fn@libero.it>
2022-07-06 15:10:58 +0200

    add posts_migrations and posts_seeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Post;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10; $i++) {
+            $new_post = new Post();
+            $new_post->title = $faker->sentence();
+            $new_post->text = $faker->paragraph();
+            $new_post->slug = Str::slug($new_post->title, '-');
+
+            $new_post->save();
+        }
+    }
+}
